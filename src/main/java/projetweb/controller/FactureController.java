cc47ce78c1b9e7a6689168890539c38ac7a208b7
zine-el-abidine.khol <zine-el-abidine.kholladi@uha.fr>
2014-11-05 18:00:58 +0100

    facture

--- src/main/java/projetweb/controller/FactureController.java
+++ src/main/java/projetweb/controller/FactureController.java
@@ -1,5 +1,6 @@
 package projetweb.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import projetweb.model.Employes;
 import projetweb.model.Facture;
 import projetweb.model.Produits;
 import projetweb.repository.FactureRepository;
@@ -26,7 +28,7 @@ public class FactureController {
 
  
 		@RequestMapping(value = "/caisse", method = RequestMethod.GET)
-		public String caisseemploye(Model model) {
+		public String caisseemploye(Model model, @ModelAttribute Produits produit) {
 			model.addAttribute("prod", new Produits());
 			model.addAttribute("products", ProduitsRepository.findAll());	
 			
@@ -36,6 +38,17 @@ public class FactureController {
 			List<Facture> l = (List<Facture>) FactureRepository.findAll();
 			if (l != null && !l.isEmpty())
 				model.addAttribute("facturess", l);
+			
+			
+	/*		List<Produits> listproduit = (List<Produits>) ProduitsRepository.findAll();
+			int sizeListe=listproduit.size();
+			int i=0;
+			while(i<sizeListe){
+				System.out.println(produit.getNom());
+				i++;		
+			}
+			*/
+			
 
 			return "caisse";
 		}	
