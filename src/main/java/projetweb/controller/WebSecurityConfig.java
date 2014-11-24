ad406a2ec49dca373945ae7e3099b64571299b4c
imaddv <imaddv@imaddv-dv6-PC>
2014-11-24 22:07:34 +0100

    liste check

--- src/main/java/projetweb/controller/WebSecurityConfig.java
+++ src/main/java/projetweb/controller/WebSecurityConfig.java
@@ -1,5 +1,8 @@
 package projetweb.controller;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
@@ -7,11 +10,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
+import projetweb.model.Employes;
+import projetweb.repository.EmployesRepository;
+
 @Configuration
 @EnableWebMvcSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	 @Override
 	protected void configure(HttpSecurity http) throws Exception {
+		 
+	
 		http.authorizeRequests().antMatchers("/","/accueil","/accueilemploi","/historique","/caisse","/loginemploi","/index","/cai","/deletefacture","/editfacture","/facture").permitAll()
 				.anyRequest().authenticated();
 		http.csrf().disable();
@@ -25,6 +33,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Configuration
 	protected static class AuthenticationConfiguration extends
 			GlobalAuthenticationConfigurerAdapter {
+		
+		private EmployesRepository EmployesRep;
+		ArrayList<Employes>listEm=new ArrayList<Employes>();
+		
 
 		@Override
 		public void init(AuthenticationManagerBuilder auth) throws Exception {
